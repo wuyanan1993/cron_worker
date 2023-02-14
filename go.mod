ebd5b8b429e6028e5c668ff1e6204d6791d07d48
吴亚楠 <wuyanan@wuyanandeMac-Studio.local>
2023-02-14 23:09:11 +0800

    Init commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module cron_worker
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+	InitTask()
+}
+
+func InitTask() {
+	// 初始化任务
+	//ticker := time.NewTicker(3 * time.Second)
+	//for range ticker.C {
+	//	fmt.Println("重复任务执行中")
+	//}
+	// 初始化channel
+	// 初始化任务生成器
+	// 初始化任务执行器
+	// 初始化任务列表，并且暴漏http 接口，可以通过api 来进行列表修改
+}
