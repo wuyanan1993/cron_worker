0c3965e04e9f0b64d1d4d1f1d40f3d40c019beb5
wuyanan.yarn <wuyanan.yarn@bytedance.com>
2023-02-16 16:41:49 +0800

    feat: 确定执行框架

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"cron_worker/tasks"
+	"fmt"
+	"time"
+)
+
 func main() {
 	InitTask()
 }
@@ -11,7 +17,26 @@ func InitTask() {
 	//	fmt.Println("重复任务执行中")
 	//}
 	// 初始化channel
-	// 初始化任务生成器
-	// 初始化任务执行器
+	taskChannel := make(chan tasks.Task, 10000)
 	// 初始化任务列表，并且暴漏http 接口，可以通过api 来进行列表修改
+	taskList := tasks.GetCronTaskList()
+	// 初始化任务生成器
+	taskProducerTicker := time.NewTicker(3 * time.Second)
+	go func() {
+		for range taskProducerTicker.C {
+			fmt.Println("模拟巡检任务生成中。。。")
+			//根据taskList循环写入到task channel里边
+			// TODO: 是否控制写入的速度，如果消费积累了很多，可以暂时不往队列里放任务，加一个判断
+			for _, v := range taskList {
+				taskChannel <- v
+			}
+		}
+	}()
+	// 初始化任务执行器
+	for {
+		task := <-taskChannel
+		// 此处应该拿到 票据才能执行，需要控制并发度
+		go task.Run()
+	}
+
 }
--- tasks/task.go
+++ tasks/task.go
@@ -1,5 +1,43 @@
 package tasks
 
+import "fmt"
+
 type Task interface {
 	Run()
+}
+
+type ReplicationLagChecker struct {
+	TaskName string
+	TaskType string
+}
+
+func (t *ReplicationLagChecker) Run() {
+	fmt.Println("正在执行ReplicationLagChecker!")
+}
+
+type BinVersionChecker struct {
+	TaskName string
+	TaskType string
+}
+
+func (t *BinVersionChecker) Run() {
+	fmt.Println("正在执行BinVersionChecker!")
+}
+
+type ShardTopologyChecker struct {
+	TaskName string
+	TaskType string
+}
+
+func (t *ShardTopologyChecker) Run() {
+	fmt.Println("正在执行ShardTopologyChecker!")
+}
+
+func GetCronTaskList() []Task {
+	taskList := []Task{
+		&ShardTopologyChecker{TaskName: "shardTopology"},
+		&ReplicationLagChecker{TaskName: "replicationLag"},
+		&BinVersionChecker{TaskName: "binVersion"},
+	}
+	return taskList
 }
